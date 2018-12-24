bf35265e04de049ed3451f0111fe1c73f5e4caf3
Raeed Asif <r.asif3@share.epsb.ca>
2018-12-23 22:36:56 -0700

    Added Lesson file

--- JavaTest.java
+++ JavaTest.java
@@ -2,15 +2,17 @@
 import java.util.*;
 
 class JavaTest {
-public static void print(String message) {System.out.println(message);};
+	public static void Weed(String message) {
+		System.out.println(message);
+	};
 
-    public static void main(String[] args) {
-    
-    
-    
-print("Test");
+	public static void main(String[] args) {
 
 
 
-    }
+		Weed("Im smoking weed");
+
+
+
+	}
 }
