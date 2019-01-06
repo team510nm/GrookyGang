bd6b13db3b9d3ca0e8fc611ccc4e35c325af39b9
M0R <chinmaytlc@gmail.com>
2019-01-06 11:21:07 -0700

    DId Some more with the main file

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 '''
 @Title of program: Robotics Java Tutorial
 @Author: Chinmay Patil
-@Date of last edit: <2019-01-04 21:40:12>
-@Amount of saves: <21>
+@Date of last edit: <2019-01-05 21:23:40>
+@Amount of saves: <24>
 '''
 from reference import *
 from parse import *
+from sys import popen
 
 # Template to use in the reference file
 ##req##requirementsDone = True ## Uncomments after requirements file finishes completly
 
-if requirementsDone != True:
+if requirementsDone:
 	from requirements import reqComplete # run requirements file
 
-if (requirementsDone == True) or (reqComplete == True):
+if requirementsDone or reqComplete:
 	print("Actual Code here")
 
 
@@ -23,3 +24,50 @@ if (requirementsDone == True) or (reqComplete == True):
 
 	lesson = parseLesson(choice)
 
+	### SUB LESSON MENU HERE ###
+	ln = "1" # lesson number
+
+	print("You are doing lesson {}".format(ln))
+
+	print(lesson[ln]["Text"])
+
+	input("Press [Enter] to continue onto the testing portion.")
+
+
+	### // TESTING PORTION // ###
+
+	assignment = lesson[ln]["Requirements"]
+
+	beforeCode = preCode
+	beforeCode += "\n\n/*\nYour requirements for this assignment are:{}*/\n\n".format(assignment)
+	beforeCode += reminder
+
+	tempFile = open("TempEditor.java","w")
+	tempFile.write(beforeCode)
+	tempFile.close()
+
+
+	# User Input Here
+	system("nano TempEditor.java -\$wS -m -l")
+	tempFile = open("TempEditor.java")
+	userCode = tempFile.read()
+	tempFile.close()
+	system("rm TempEditor.java")
+
+	code = lesson[ln]["Code"]
+
+	code = code.replace("// Code Here", userCode)
+
+
+	openFile = open(".running/main.java","w")
+	openFile.write(code)
+	openFile.close()
+
+	print("")
+	building = popen("cd .running && javac main.java")
+
+	if popen == "":
+		print("An error arose during the compiling of your program:")
+		print(building)
+	else:
+		pass
