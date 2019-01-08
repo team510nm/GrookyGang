a86d8ba8073d44907c1af701f149d33f8ca93b94
M0R <chinmaytlc@gmail.com>
2019-01-08 11:20:50 -0700

    Fixed ';'

--- .running/main.java
+++ .running/main.java
@@ -17,7 +17,7 @@ To exit, press [Ctrl+X]
 /*
 Your requirements for this assignment are:
 For your first task, create a method that squares a number then returns the squared number.
-For your second task, create a main method (called 'runOpMode') that runs the method from the first task, inputting the number 5.
+For your second task, create a FTC main method (called 'runOpMode') that runs the method from the first task, inputting the number 5.
 For you third task, create a DcMotor object in the main method and set its power to the output of the previous task.
 */
 
@@ -25,6 +25,19 @@ For you third task, create a DcMotor object in the main method and set its power
 Remember to add a ; at the end of every line.
 Start coding after this comment ends.
 */
+public static int s(int args) {
+return args*args;
+}
+
+public static void runOpMode() {
+DcMotor m = new DcMotor();
+m.setPower(s(5));
+}
+
+
+	public static void main(String[] args) {
+	    runOpMode();
+	}
 }
 
 
