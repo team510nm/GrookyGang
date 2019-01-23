4a9f5c09291a8d89f1e400cf2b4c46f7a909a83b
Chinmay Patil <c.patil@share.epsb.ca>
2019-01-22 17:00:53 -0700

    Added Comments to main file

--- main.py
+++ main.py
@@ -8,40 +8,47 @@ from reference import *
 from parse import *
 from menu import *
 from os import popen,system
+'''
+Template to use in the reference file
+requirementsDone = True ## Uncomments after requirements file finishes completly
+'''
 
-# Template to use in the reference file
-##req##requirementsDone = True ## Uncomments after requirements file finishes completly
-
+# If the requirements are not done
 if not requirementsDone:
-	from requirements import reqComplete # run requirements file
+	from requirements import reqComplete # run requirements file and try to get if they had the requirements
 
+    
+# If requirements have been done
 if requirementsDone or reqComplete:
 
 	while True:
 		system("clear")
-		### MENU HERE (Or something) ###
+
+		### Main menu ###
 		choice = mainMenu(validLessons)
 
-		#choice = "Lesson1"
+		# Example of an output of mainMenu:
+		# choice = "Lesson1"
 
 
+		# Change lesson
 		lesson = parseLesson(choice)
 
 
 		while True:
-			### SUB LESSON MENU HERE ###
-			#ln = "1" # lesson number
+
+			### SUB LESSON MENU###
 			system("clear")
 			ln = submenu(lesson)
-			if ln == "0":
+			if ln == "0": # If they select to exit, break out of while loop back to main menu.
 				break
 
 			system("clear")
-			print("You are doing lesson {}".format(ln))
+			print("You are doing {}, sub-lesson {}".format(choice,ln))
 
-			print(lesson[ln]["Text"])
+			print(lesson[ln]["Text"]) # Print lesson
 
-			input("Press [Enter] to continue onto the testing portion.")
+			input("Press [Enter] to continue onto the testing portion.") # Wait for enter
 
 
 			### // TESTING PORTION // ###
@@ -49,8 +56,9 @@ if requirementsDone or reqComplete:
 			while True:
 				print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-				assignment = lesson[ln]["Requirements"]
+				assignment = lesson[ln]["Requirements"] # Get assignment details
 
+				# Add assignment, and reminders into the code editor (in a comment)
 				beforeCode = preCode
 				beforeCode += "\n\n/*\nYour requirements for this assignment are:{}*/\n\n".format(assignment)
 				beforeCode += reminder
@@ -60,47 +68,48 @@ if requirementsDone or reqComplete:
 				tempFile.close()
 
 
-				# User Input Here
-				system("nano TempEditor.java -\$wS -m -l")
+				system("nano TempEditor.java -\$wS -m -l") # user inputs code in here
+
+				# Read user's code then remove the editor file
 				tempFile = open("TempEditor.java")
 				userCode = tempFile.read()
 				tempFile.close()
 				system("rm TempEditor.java")
 
-				code = lesson[ln]["Code"]
+				code = lesson[ln]["Code"] # Get code template
 
-				code = code.replace("// Code Here", userCode)
+				code = code.replace("// Code Here", userCode) # Input user code into the code template
 
 
-				openFile = open(".running/main.java","w")
+				openFile = open(".running/main.java","w") # write code into the running file
 				openFile.write(code)
 				openFile.close()
 
-				print("")
+				print("") # Build the file, if success, print out complete so we can tell it finished
 				building = popen("cd .running && javac main.java && echo 'complete'").read()
 
-				#print(building)
 				validYes = ["y", ""]
 
-				if not building == "complete\n":
+				if not building == "complete\n": # If the building was not completed tell user
 					print("")
 					print("An error arose during the compiling of your program (Visible above)")
-					#print(building)
+					# Ask user weither or not to exit to lesson menu
 					redo = input("Would you like to redo this test? (Y/n) : ").lower()
 					if not (redo in validYes):
-						break
+						break # break out of testing loop to sub-lesson menu loop
 				else:
-					out = popen("cd .running && java main").read()
-					expectedOut = lesson[ln]["Output"][1:]
-					if expectedOut == out:
-						print("You passed the test.")
+					out = popen("cd .running && java main").read() # Run the file
+					expectedOut = lesson[ln]["Output"][1:] # Get the expected output
+					if expectedOut == out: # If the expected output is the same as the output
+						print("You passed the test.") # Tell user they passed, then exit the testing loop
 						break
 					else:
 						print("You failed the test, the following was the output of your code:")
 						print(out)
+						# Ask user weither or not to exit to lesson menu
 						redo = input("Would you like to redo this test? (Y/n) : ").lower()
 						if not (redo in validYes):
-							break
+							break # break out of testing loop to sub-lesson menu loop
 				print("\n")
 			print("")
-			input("Press [Enter] to return to menu.")
+			input("Press [Enter] to return to menu.") # Confirmation to exit back to the sub-lesson menu
