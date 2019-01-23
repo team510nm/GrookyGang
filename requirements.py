84dbab0aebbf3bd5e7a607521a5c764e07e6b2b5
Chinmay Patil <c.patil@share.epsb.ca>
2019-01-22 17:12:46 -0700

    Added Comments to the Requirements file

--- requirements.py
+++ requirements.py
@@ -2,41 +2,46 @@
 @Title of program: requirements
 @Author: Chinmay Patil
 @Date of creation: 2018-12-29 - 19:41
+
+Test for if the user has the correct requirements to run the program
 """
 from os import popen,system
 from reference import *
 
-if requirementsDone != True:
+if requirementsDone != True: # If they havent already done this
 
-	reqComplete = False
+	reqComplete = False # Used when testing requirements in main file
 
+	# Java test text
 	testText = """
 	class main {
 		public static void main(String[] args) {
 			System.out.println("Test");
 		}
 	}
-	"""
+	""" 
 
+	# Nano test text
 	testText2 = """
 
 
-	If this text is displayed, go to the start of the file.
-	At the start of the file, type the following 'nano' (without quotes)
+If this text is displayed, go to the start of the file.
+At the start of the file, type the following without quotes: 'nano'
 	"""
+
+
+
 
 	testFile = open(testFile,"w")
 	testFile.write(testText)
 	testFile.close()
 
-	system("cd .running && javac JavaTest.java")
-	systemLog = popen("cd .running && java main").read()
-	remove = popen("cd .running && rm *.class").read()
+	system("cd .running && javac JavaTest.java") # Test for JDK
+	systemLog = popen("cd .running && java main").read() # Test for JRE
+	remove = popen("cd .running && rm *.class").read() # Test for bash
 	system("cd .running && rm JavaTest.java")
-	#remove = ""
-	#systemLog = 'Test\n'
 
-	if systemLog != "Test\n": #(remove == "rm: *.class: No such file or directory") or
+	if systemLog != "Test\n": # If test didn't pass
 		print("Sorry, you appear to either not be using a Unix device, or be missing the Java JDK")
 		print("Please install either bash or the latest openJDK.")
 		print("For more instructions, please follow the link below:")
@@ -58,13 +63,15 @@ if requirementsDone != True:
 
 
 
-		if text[:4] == "nano":
+		if text[:4] == "nano": # if first bit of file says nano
 			print("Congratulations, you have passed all checks, you are now able to use the program.")
 			print("We respect your time, and as such, you will only be made to do this test once per computer.")
 			print("To redo this test, please run the requirements file included.")
 			print("Thank you for your time.")
 			print("")
 
+
+			# Change requirements variable from False to True in the reference file
 			referenceFile = open("reference.py","r")
 			reference = referenceFile.read()
 			reference = reference.replace("requirementsDone = False","requirementsDone = True")
@@ -74,11 +81,11 @@ if requirementsDone != True:
 			referenceFile.write(reference)
 			referenceFile.close()
 
-			reqComplete = True
+			reqComplete = True # Main file will try to import this variable (when importing file runs)
 
 			input("Please press [Enter] to proceed.")
 
-		else:
+		else: # If first bit of file says anything else (or nothing)
 			print("Sorry, you appear to be having issues with nano, the text editor we use.")
 			print("If you are opening this from inside an IDE, then it will also not work.")
 			print("Please either install nano, or run this within a proper terminal.")
